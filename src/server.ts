c975d037b7ac02f67d286d71e17df086dca8f759
igortavtib <igortavtib@gmail.com>
2020-09-14 19:37:22 -0300

    Initinal commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json())
+
+app.use(routes);
+
+app.get('/', (request, response) => {
+  return response.json({message: 'Hellow GoStack!'})
+})
+
+app.listen(3333, () => {
+  console.log('Server turned on 💕')
+})
